65a640b708b6f9f8b094f1455344a8dbcc60bc44
Venice Anne Manacop <25-00261@g.batstate-u.edu.ph>
2025-09-18 20:38:24 +0800

    Add loop to print love message ten times

---
+++ mahal-kita-loop.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    int love = 1;
+
+    while (love <= 10) {
+        cout << love << ". Mahal kita dahil mahal kita" << endl;
+        love++;
+    }
+
+    /* 
+        while (love >= 1) {
+        cout << love << endl;
+        love--;
+    }
+        
+    */
+    return 0;
+}
